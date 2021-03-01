d9f78b69043a707bf8ba6e8be87c702e50f9a72f
Cyber Hawk <cs.cybernetic@gmail.com>
2021-03-01 14:03:12 +0500

    Update script.js

--- Assignment # 15/5. for Loop/Program # 03/script.js
+++ Assignment # 15/5. for Loop/Program # 03/script.js
@@ -1,8 +1,17 @@
-function printAlphabets() {
+function printLowerAlphabets() {
     let arr = [];
     for (let i = 97; i <= 122; i++) {
         let alph = ' ' + String.fromCharCode(i);
         arr.push(alph);
     }
-    document.getElementById("ans").innerHTML = arr;
+    document.getElementById("small").innerHTML = arr;
+}
+
+function printUpperAlphabets() {
+    let arr = [];
+    for (let i = 65; i <= 90; i++) {
+        let alph = ' ' + String.fromCharCode(i);
+        arr.push(alph);
+    }
+    document.getElementById("cap").innerHTML = arr;
 }
